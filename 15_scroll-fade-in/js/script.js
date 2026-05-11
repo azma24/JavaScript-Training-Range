74a599688721c09aa086a736d7a8e6ffabd0479e
azma24 <278310366+azma24@users.noreply.github.com>
2026-05-11 16:06:44 +0900

    Add scroll-fade-in

---
+++ 15_scroll-fade-in/js/script.js
@@ -0,0 +1,11 @@
+const target = document.querySelector('.fade-target');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+observer.observe(target);
